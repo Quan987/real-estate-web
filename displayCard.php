607a7cabc8519148918c01b26071a0e262150e39
CookieStudios2600 <78653311+CookieStudios5609@users.noreply.github.com>
2023-12-07 12:53:03 -0500

    Feat: Begin styling displayCard

--- displayCard.php
+++ displayCard.php
@@ -1,15 +1,27 @@
 <?php require('./db.php'); 
     $card = getCardByAddr($_POST["location"]);
 ?>
-<?=
-    "<div>" .
-        "<img src=" . $card['img'] . ">" .
-        "<h1>" . $card["addr"] . "</h1>" .
-        "<h2>$" . $card["price"] . " • " . $card["age"] . " years old</h2>" . 
-        "<p>Sold by: " . $card["seller"] . 
-        "<p>" . $card["beds"] . "bedrooms, " . $card["baths"] . "bathrooms</p>" . 
-        "<p>" . $card["garage"] . "-car garage</p>" .
-        "<p>" . $card["areaL"] . "sq. ft. by " . $card["areaW"] . "sq. ft.</p>"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <?=
+        "<div class='dashboard'>" .
+            "<img src=" . $card['img'] . ">" .
+            "<h1>" . $card["addr"] . "</h1>" .
+            "<h2>$" . $card["price"] . " • " . $card["age"] . " years old</h2>" . 
+            "<p>Sold by: " . $card["seller"] . 
+            "<p>" . $card["beds"] . " bedrooms, " . $card["baths"] . " bathrooms</p>" . 
+            "<p>" . $card["garage"] . "-car garage</p>" .
+            "<p>" . $card["areaL"] . "sq. ft. by " . $card["areaW"] . "sq. ft.</p>"
 
-    . "</div>";
-?>
+        . "</div>";
+    ?>
+</body>
+</html>
